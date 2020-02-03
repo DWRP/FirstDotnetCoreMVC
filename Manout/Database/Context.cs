b32a175d782b7d53240c7ea27bbfa776d8cd7128
Douglas Walas <douglas.skd@gmail.com>
2020-02-03 20:07:09 -0300

    Adicionando arquivos ao git

---
+++ Manout/Database/Context.cs
@@ -0,0 +1,19 @@
+﻿using Manout.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manout.Database
+{
+    public class Context : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=localhost\SQLEXPRESS;Initial Catalog=manout;Integrated Security=True");
+        }
+    }
+}
---
+++ Manout/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manout.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name ="Descrição")]
+        [Required(ErrorMessage ="Obrigatório")]
+        public string Descricao { get; set; }
+        [Range(1,999,ErrorMessage ="Value out of Range!")]
+        public int Quantidade { get; set; }
+        [Required(ErrorMessage = "Obrigatório")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
